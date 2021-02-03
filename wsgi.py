6e537f385516dd4f6aeb1109992b80161c2b1b79
ananthasubramanianv <77930307+ananthasubramanianv@users.noreply.github.com>
2021-02-03 15:49:31 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -6,4 +6,4 @@ if project_home not in sys.path:
     sys.path = [project_home] + sys.path
 
 # import flask app but need to call it "application" for WSGI to work
-from application import app as application 
+from polvalcalc import app as application 
